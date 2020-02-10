e9a6a99e320ac93ba591a8a5935eb4889df79236
khaganii <xaqaniabdullayev99@gmail.com>
2020-02-10 19:04:21 +0400

    calculator

---
+++ src/main/java/simple_calc.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class simple_calc {
+        public static void main(String[] args){
+            Scanner input = new Scanner(System.in);
+            System.out.println("First_num: ");
+            int a = input.nextInt();
+            System.out.println("Second_num");
+            int b = input.nextInt();
+            System.out.println("Sum_of_them: " + (a+b));
+        }
+}
