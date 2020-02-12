c378791cbfb28c4fea6b6f0e2de3a7ff317afcb3
khaganii <xaqaniabdullayev99@gmail.com>
2020-02-12 17:05:28 +0400

    sum of array elements generation

--- src/main/java/app/sum_of_array.java
+++ src/main/java/app/sum_of_array.java
@@ -14,16 +14,29 @@ public class sum_of_array {
         System.out.println("elements of array1: ");
         for (int i = 0; i<20; i++)
         {
-            arr1[i] = random.nextInt(20 + 20) - 20;
-            if(arr1[i] % 2 == 0) evensum = evensum + arr1[i];
+            while (true)
+            {
+                int k = random.nextInt(20 + 20) - 20;
+                if (k % 2 == 0) {
+                    arr1[i] = k;
+                    break;
+                }
+            }
+            evensum = evensum + arr1[i];
             System.out.print(arr1[i] + " ");
         }
         System.out.println();
         System.out.println("elements of array2: ");
         for (int i = 0; i<30; i++)
         {
-            arr2[i] = random.nextInt(30 + 30) - 30;
-            if(arr2[i] % 2 != 0) oddsum = oddsum + arr2[i];
+            while (true)
+            {
+                int k = random.nextInt(30 + 30) - 30;
+                if (k % 2 == 1) {
+                    arr2[i] = k;
+                    break;
+                }
+            }
             System.out.print(arr2[i] + " ");
         }
         System.out.println();
